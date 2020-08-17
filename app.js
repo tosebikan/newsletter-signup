18e605346db96de27876dff90391b1e895601c69
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-17 17:22:11 +0000

    body parser added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
