1c48cb1244faefbd6d5817ffd3ceb7e4dca84169
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-17 17:25:06 +0000

    post method backend

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true});
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", (req, res)=>{
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email)
+})
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
